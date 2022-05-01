50c786fbb663827d8f989932e0e65174c2be062a
shawn00-tech <shawnjay001@gmail.com>
2022-05-01 14:44:02 +0545

    Update train.py

--- DCGAN/train.py
+++ DCGAN/train.py
@@ -13,7 +13,7 @@ from model import Generator, Discriminator, initialize_weights
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 LEARNING_RATE = 2e-4
-BATCH_SIZE = 128
+BATCH_SIZE = 32
 IMAGE_SIZE = 64
 CHANNELS_IMG = 1
 Z_DIM = 100
@@ -22,6 +22,28 @@ FEATURES_DISC = 64
 FEATURES_GEN = 64
 
 
+transforms = transforms.Compose([
+    transforms.Resize(IMAGE_SIZE),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        [0.5 for _ in range(CHANNELS_IMG)],[ 0.5 for _ in range(CHANNELS_IMG)]),
+
+]
+)
+
+dataset = datasets.MNIST(root=./data, train=True, transform=transforms, download=True)
+loader = torch.utils.data.DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
+
+gen = Generator(Z_DIM, CHANNELS_IMG, FEATURES_GEN).to(device)
+disc = Discriminator(CHANNELS_IMG, FEATURES_DISC).to(device)
+initialize_weights(gen)
+initialize_weights(disc)
+
+opt_gen = optim.Adam(gen.parameters(), lr=LEARNING_RATE, betas=(0.5, 0.999))
+opt_disc = optim.Adam(disc.parameters(), lr=LEARNING_RATE, betas=(0.5, 0.999))
+criterion = nn.BCELoss()
+
+noise = torch.randn(BATCH_SIZE, Z_DIM, 1, 1).to(device)
 
 
 
