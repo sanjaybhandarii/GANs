37f8dc0711fe0c951f6b30bd682463956adb385c
shawn00-tech <shawnjay001@gmail.com>
2022-05-03 11:10:01 +0545

    Initial commit

--- practice.py
+++ practice.py
@@ -1,5 +1,14 @@
 import torch 
 
-a = torch.randn(4,1,1,1).repeat(1,2,2,2)
-print(a.shape)
-print(a.view(a.shape[0],-1))
+a = torch.randn(4,1,2,2)
+b= torch.randint(0,1,(4,1))
+embed = torch.nn.Embedding(2,4)
+c = embed(b)
+print(c)
+d= c.view(4,1,2,2)
+
+print("shape of a",a.shape)
+
+print("shape of b",b.shape)
+print("shape of c",c.shape)
+print("shape of d",d.shape)
